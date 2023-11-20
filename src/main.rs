b642ad1c7d804c5630acd41db46bfe43d94c58c2
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-11-20 11:43:57 +0100

    add: struct for config

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,31 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
     // Storing information
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
 
     // Read file 
-    let content = fs::read_to_string(file_path).expect("ERROR: Wrong filepath given");
+    let content = fs::read_to_string(config.file_path).expect("ERROR: Wrong filepath given");
 
     println!("With content: {content}");
 
 
 
+}
+
+
+struct Config{
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
