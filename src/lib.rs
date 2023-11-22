418be6121aa202e4af7cc06cc8c535a5fc123355
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-11-22 15:46:52 +0100

    add: search function to run for the ability to search

--- src/lib.rs
+++ src/lib.rs
@@ -21,13 +21,12 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
     // Read file 
     let content = fs::read_to_string(config.file_path)?;
 
-    println!("With content: {content}");
+    for line in search(&config.query, &content){
+        println!("{line}");
+    }
 
     Ok(())
 
