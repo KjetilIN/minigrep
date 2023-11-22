f68832f4e9d0a1b7aef7be1867fc5cd9df5138b7
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-11-22 12:20:53 +0100

    add: create run function + error handling for config and run

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
 use std::env;
 use std::fs;
-
-fn main() {
-    // Grab the arguments 
-    let args: Vec<String> = env::args().collect();
-    
-    // Storing information
-    let config = Config::new(&args);
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-
-    // Read file 
-    let content = fs::read_to_string(config.file_path).expect("ERROR: Wrong filepath given");
-
-    println!("With content: {content}");
-
-
-
-}
-
+use std::process::exit;
+use std::error::Error;
 
 struct Config{
     query: String,
@@ -28,16 +9,51 @@ struct Config{
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         // Check if we have gotten 3 arguments at least
         if args.len() < 3{
-            panic!("Not enough arguments given")
+            return Err("Not enough arguments");
         }
-
+        
         let query = args[1].clone();
         let file_path = args[2].clone();
     
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    // Read file 
+    let content = fs::read_to_string(config.file_path)?;
+
+    println!("With content: {content}");
+
+    Ok(())
+
+}
+
+fn main() {
+    // Grab the arguments 
+    let args: Vec<String> = env::args().collect();
+    
+    // Storing information
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("ERROR: not valid arguments : {err}");
+        exit(1);
+    });
+
+    // Running the config
+    if let Err(e) =  run(config){
+        println!("ERROR: error with the provided arguments: {e}");
+        exit(1);
+    }
+
+}
+
+
+
 
