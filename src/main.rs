0fe06ac4a8844da7c08a57b0dd963d3e690403c8
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-11-22 12:26:38 +0100

    refactor: move config and run to a separate file (lib.rs)

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,8 @@
 use std::env;
-use std::fs;
 use std::process::exit;
-use std::error::Error;
 
-struct Config{
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        // Check if we have gotten 3 arguments at least
-        if args.len() < 3{
-            return Err("Not enough arguments");
-        }
-        
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-    
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    // Read file 
-    let content = fs::read_to_string(config.file_path)?;
-
-    println!("With content: {content}");
-
-    Ok(())
-
-}
+// Get the Config and run function from lib.rs
+use minigrep::{Config, run};
 
 fn main() {
     // Grab the arguments 
